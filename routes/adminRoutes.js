61f3192df49d078abf376421f1d8942a6fc4246a
Sameer <banthwa013@gmail.com>
2025-07-06 21:54:39 +0545

    more dashboard stuff

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-router.get('/dashboard', adminController.getDashboard);
-
+router.get('/stats', adminController.getDashboard);
+router.get('/users', adminController.getUsers);
+router.get('/users', (req, res) => {
+  res.render('dashboard/users', { title: 'Users', user: req.session.user });
+});
 module.exports = router;
